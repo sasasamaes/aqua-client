bfde5005a1de63eb5122404168b740de612824b2
Francisco Campos <franciscocampos@Franciscos-MacBook-Air.local>
2021-12-15 15:19:35 -0600

    fix

--- src/components/service.js
+++ src/components/service.js
@@ -4,14 +4,12 @@ import { Col, Row } from 'react-bootstrap'
 const ServiceComponent = ({ services, lg }) => {
   return (
     <Col lg={lg} className="service-container bg-dark">
-      <Row>
-        {services &&
-          services.map((service, index) => (
-            <Col className="service-item" key={`service-item-${index}`}>
-              <h3>{service.title}</h3>
-            </Col>
-          ))}
-      </Row>
+      {services &&
+        services.map((service, index) => (
+          <Col className="service-item" key={`service-item-${index}`}>
+            <h3>{service.title}</h3>
+          </Col>
+        ))}
     </Col>
   )
 }
