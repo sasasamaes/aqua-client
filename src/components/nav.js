cf6e32c62fb7e0ecfe5cb1d1a9b6d5c5800b12e6
Francisco Campos <franciscocampos@Franciscos-MacBook-Air.local>
2021-12-15 15:29:29 -0600

    nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,16 +4,10 @@ import { Container, Navbar, Row, Col } from 'react-bootstrap'
 const Nav = ({ global }) => (
   <Navbar fixed="top" bg="dark" variant="dark">
     <Container fluid id="navbar">
-      <Row>
-        <Col lg={4}>
-          <Navbar.Brand href="/">
-            <img src={global?.logo?.url} alt={global?.logo?.caption} />
-          </Navbar.Brand>
-        </Col>
-        <Col lg={6}>
-          <p>{global ? global?.navTitle : ''}</p>
-        </Col>
-      </Row>
+      <Navbar.Brand href="/" className="navbar-brand">
+        <img src={global?.logo?.url} alt={global?.logo?.caption} />
+        <p>{global ? global?.navTitle : ''}</p>
+      </Navbar.Brand>
     </Container>
   </Navbar>
 )
